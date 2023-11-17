f971bee76e9c8f34e263bc6e21d196d75aac4387
Alexander Petrovski <alexanderpetrovski@Alexanders-MacBook-Pro.local>
2023-11-17 14:51:32 -0500

    add git pages url

--- server.js
+++ server.js
@@ -10,7 +10,11 @@ const app = express();
 require("dotenv").config();
 
 const corsOptions = {
-  origin: ["http://localhost:3001", "http://127.0.0.1:3001"],
+  origin: [
+    "http://localhost:3001",
+    "http://127.0.0.1:3001",
+    "https://alexanpetrov.github.io/gastrograph/",
+  ],
   credentials: true,
   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
 };
