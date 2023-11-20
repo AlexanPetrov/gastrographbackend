a4fc41fdb9b71ce508d5289a4d44bb9605614a73
Alexander Petrovski <alexanderpetrovski@Alexanders-MacBook-Pro.local>
2023-11-20 16:08:44 -0500

    photes from db

--- controllers/recipes.js
+++ controllers/recipes.js
@@ -2,7 +2,9 @@ const Recipe = require("../models/Recipe.js");
 
 const getAllRecipes = async (req, res) => {
   try {
+    console.log("Fetching recipes..."); // Add this log
     const recipes = await Recipe.find();
+    console.log("Fetched recipes:", recipes); // Add this log
     const adaptedRecipes = recipes.map((recipe) => {
       return {
         id: recipe._id.toString(),
@@ -16,6 +18,7 @@ const getAllRecipes = async (req, res) => {
     });
     res.json(adaptedRecipes);
   } catch (error) {
+    console.error("Error fetching recipes:", error); // Add this log
     res.status(500).json({ error: error.message });
   }
 };
