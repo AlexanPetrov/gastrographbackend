2f83b3c813afcb861b4010075b5a02cdddb4d08b
Alexander Petrovski <alexanderpetrovski@Alexanders-MacBook-Pro.local>
2023-11-27 14:10:40 -0500

    edit .404

--- routes/auth.js
+++ routes/auth.js
@@ -9,16 +9,7 @@ const {
 
 router.post("/signup", signup);
 router.post("/login", login);
-router.post("/request-reset-password", requestPasswordReset); // New route for requesting password reset
-router.post("/reset-password", resetPassword); // New route for resetting the password using the token
+router.post("/request-reset-password", requestPasswordReset);
+router.post("/reset-password", resetPassword);
 
 module.exports = router;
-
-// const express = require("express");
-// const router = express.Router();
-// const { signup, login } = require("../controllers/auth.js");
-
-// router.post("/signup", signup);
-// router.post("/login", login);
-
-// module.exports = router;
